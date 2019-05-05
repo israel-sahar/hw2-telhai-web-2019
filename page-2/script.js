8cca90281f0571bf30c763f4634f820ed81c1963
israel-sahar <37867238+israel-sahar@users.noreply.github.com>
2019-05-05 09:53:26 +0300

    Update script.js

--- page-2/script.js
+++ page-2/script.js
@@ -1,24 +1,30 @@
+function isNumber(val, base) {
+    do{
+        if(val%10>=base) return false;
+        val=val/10;
+    }while(val!=0);
 
-            function ConvertBase(val, base1, base2) {
-                if (typeof(val) == "number") {
-                    return parseInt(String(val)).toString(base2);
-                } else {
-                    return parseInt(val.toString(), base1).toString(base2)
-                };
-            }
-            
-            function checkInput()
-            {
-                var userInput,sbox,text;
-                userInput =Number( document.getElementById("input").value);
-                if(isNaN(userInput)||userInput == "")
-                {
-                text = "not a valid input";
-                document.getElementById("input").value = "";
-                document.getElementById("input").placeholder = text;
-                }
-                else
-                {
-                document.getElementById("inputOut").value = ConvertBase(document.getElementById("input").value,document.getElementById("selectFromBox").value,document.getElementById("selectToBox").value)
-                }
-            }
+    return true;
+}
+function ConvertBase(val, base1, base2) {
+    if (typeof (val) == "number") {
+        return parseInt(String(val)).toString(base2);
+    } else {
+        return parseInt(val.toString(), base1).toString(base2)
+    };
+}
+
+function checkInput() {
+    var userInput, sbox, text;
+    userInput = Number(document.getElementById("input").value);
+    if (isNaN(userInput) || userInput == "" || !isNumber(userInput,Number(document.getElementById("selectFromBox").value))) {
+        text = "not a valid input";
+        document.getElementById("input").value = "";
+        document.getElementById("input").placeholder = text;
+        document.getElementById("inputOut").value=""
+        document.getElementById("inputOut").placeholder="The answer is:"
+    }
+    else {
+        document.getElementById("inputOut").value = ConvertBase(document.getElementById("input").value, document.getElementById("selectFromBox").value, document.getElementById("selectToBox").value)
+    }
+}
